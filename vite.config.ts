bd1ce5f5c9f6479717f1c219d9afd77d860a555f
liuyun <yunliuly@cn.ibm.com>
2022-10-11 12:19:17 +0800

    feat: 添加插件以支持script name属性

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import eslint from 'vite-plugin-eslint'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueSetupExtend(),
     vueJsx(),
     eslint(),
     createSvgIconsPlugin({
